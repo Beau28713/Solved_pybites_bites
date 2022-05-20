404ae57cd19bccba40e538a4bd293bd29fb875d8
Beau <beau28713@gmail.com>
2022-05-20 10:22:41 -0400

    solved bite_80

---
+++ bite_80/equality.py
@@ -0,0 +1,44 @@
+"""In this Bite we compare two list objects for equality, a fundamental thing to understand in Python.
+
+Complete the check_equality function returning the various Enum values (representing equality scores) according to the type of equality of the lists:
+
+return SAME_REFERENCE if both lists reference the same object,
+return SAME_ORDERED if they have the same content and order,
+return SAME_UNORDERED if they have the same content unordered,
+return SAME_UNORDERED_DEDUPED if they have the same unordered content and reduced to unique items,
+and finally return NO_EQUALITY if none of the previous cases match.
+Note that == and is are not the same in Python!
+
+Have fun and keep calm and code in Python!
+"""
+
+from enum import Enum
+
+
+class Equality(Enum):
+    SAME_REFERENCE = 4
+    SAME_ORDERED = 3
+    SAME_UNORDERED = 2
+    SAME_UNORDERED_DEDUPED = 1
+    NO_EQUALITY = 0
+
+
+def check_equality(list1, list2):
+    """Check if list1 and list2 are equal returning the kind of equality.
+       Use the values in the Equality Enum:
+       - return SAME_REFERENCE if both lists reference the same object
+       - return SAME_ORDERED if they have the same content and order
+       - return SAME_UNORDERED if they have the same content unordered
+       - return SAME_UNORDERED_DEDUPED if they have the same unordered content
+         and reduced to unique items
+       - return NO_EQUALITY if none of the previous cases match"""
+    if list1 is list2:
+        return Equality.SAME_REFERENCE
+    elif list1 == list2:
+        return Equality.SAME_ORDERED
+    elif len(set(list1+list2)) == len(list1):
+        return Equality.SAME_UNORDERED
+    elif len(set(list1).symmetric_difference(set(list2))) == 0:
+        return Equality.SAME_UNORDERED_DEDUPED
+    else:
+        return Equality.NO_EQUALITY
