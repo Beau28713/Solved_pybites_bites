26bf6ddc78919fbc155c1dc6e7f11b15eec33900
Beau <beau28713@gmail.com>
2022-05-26 13:28:19 -0400

    solved bit_262

--- bite_262/gc_content.py
+++ bite_262/gc_content.py
@@ -1,4 +1,17 @@
+"""Example:
+DNA Sequence:
+AAAAAAAATTTTTTGGGGCC
+A=8, T=6, G=4, C=2, TOTAL = 20
+GC content = n(G|C) / n(A|C|G|T) = 6/20 = 3/10 => 30%
+"""
 from collections import Counter
 
 def calculate_gc_content(sequence):
+    sequence = sequence.upper()
     count = Counter(sequence)
+    n1 = count["G"] + count["C"]
+    n2 = count["A"] + count["C"] + count["G"] + count["T"]
+    gc_content = (n1 / n2) * 100
+    print(count)
+    # return round(gc_content, 2)
+    
