180ad317a81083fbe0ca80ca8778e9196a10659b
Beau <beau28713@gmail.com>
2022-05-27 22:26:12 -0400

    not finished

--- bite_225/convert_chars.py
+++ bite_225/convert_chars.py
@@ -1,7 +1,17 @@
+"""In this Bite you will swap case all pybites characters (both lower- and upper case) for a given text.
+
+Not much more as for our instruction, just complete convert_pybites_chars
+
+(Note that all converted chars are in the string pybites)
+"""
+
 PYBITES = "pybites"
 
 
 def convert_pybites_chars(text):
     """Swap case all characters in the word pybites for the given text.
        Return the resulting string."""
-    pass
+    for ele in text:
+        for char in ele:
+            if char in PYBITES:
+                "{char}".format(char.lower())
