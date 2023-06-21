fdde7187935265e0bc829c302127ccb99523d873
Beau <beau28713@gmail.com>
2023-06-21 10:39:39 -0400

    nothing new

--- bite_353/script.py
+++ bite_353/script.py
@@ -1,16 +1,16 @@
+
 import typer  # use typer.run and typer.Argument
 
 def sum_numbers(a: int, b: int):
-    """ CLI that allows you to add two numbers
-        The value of the first summand
-        The value of the second summand
-    """
     return a + b
 
 
-def main(a = typer.Argument(default=...), b = typer.Argument(default=...)):
-    
-    return sum_numbers(a, b)
+def main(a = typer.Argument(), b = typer.Argument()):
+
+    """ CLI that allows you to add two numbers
+    """
+
+    print(a + b)
 
 
 if __name__ == "__main__":
