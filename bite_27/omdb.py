7d2b022424e48ed4c79a6bedf99803a497f03041
Beau Clark <beau28713@gmail.com>
2024-01-25 16:21:41 -0500

    solved bite 27

--- bite_27/omdb.py
+++ bite_27/omdb.py
@@ -3,19 +3,38 @@ import json
 
 def get_movie_data(files: list) -> list:
     """Parse movie json files into a list of dicts"""
-    pass
-
+    movie_list = []
+    for file in files:
+        with open(file, "r") as f:
+            data = json.loads(f.read())
+            movie_list.append(data)
+    return movie_list
 
 def get_single_comedy(movies: list) -> str:
     """return the movie with Comedy in Genres"""
-    pass
+    _ = "".join([movie.get("Title") for movie in movies if "Comedy" in movie.get("Genre").split(",")])
+    
+    return _
 
 
 def get_movie_most_nominations(movies: list) -> str:
     """Return the movie that had the most nominations"""
-    pass
+    prev_nominations = 0
+    for movie in movies:
+        movie_nominations = int(movie.get("Awards").split("&")[1].split(" ")[1])
+        if(movie_nominations > prev_nominations):
+            prev_nominations = movie_nominations
+            nom_movie = movie.get("Title")
+    return nom_movie
 
 
 def get_movie_longest_runtime(movies: list) -> str:
     """Return the movie that has the longest runtime"""
-    pass
+    
+    prev_runtimes = 0
+    for movie in movies:
+        movie_runtime = int(movie.get("Runtime").split(" ")[0])
+        if(movie_runtime > prev_runtimes):
+            prev_runtimes = movie_runtime
+            runtime_movie = movie.get("Title")
+    return runtime_movie
