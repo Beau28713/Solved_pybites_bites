a1f7ce636c8b41c3f252bb0d3bd7fb2f5e2d2f31
Beau Clark <beau28713@gmail.com>
2024-02-02 17:33:09 -0500

    solved bite_36

--- bite_36/profile.py
+++ bite_36/profile.py
@@ -1,2 +1,24 @@
-def get_profile():
-    pass
+def get_profile(name: str, age: int, *args, **kwargs) -> dict:
+    if not isinstance(age, int):
+        raise ValueError
+
+    if len(args) >= 6:
+        raise ValueError
+
+    my_dict = {}
+
+    if args and kwargs:
+        my_dict.update(
+            {"name": name, "age": age, "sports": sorted([*args]), "awards": {**kwargs}}
+        )
+
+    if args:
+        my_dict.update({"name": name, "age": age, "sports": sorted([*args])})
+        return my_dict
+
+    if kwargs:
+        my_dict.update({"name": name, "age": age, "awards": {**kwargs}})
+
+    my_dict.update({"name": name, "age": age})
+
+    return my_dict
