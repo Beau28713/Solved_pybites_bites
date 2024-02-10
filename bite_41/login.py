cd999e128de82688325d75c4ff25aea0172594cb
Beau Clark <beau28713@gmail.com>
2024-02-10 09:12:54 -0500

    solved bite 41

---
+++ bite_41/login.py
@@ -0,0 +1,21 @@
+from functools import wraps
+known_users = ['bob', 'julian', 'mike', 'carmen', 'sue']
+loggedin_users = ['mike', 'sue']
+
+
+def login_required(func):
+    @wraps(func)
+    def wrapper(user):
+        if user in loggedin_users:
+            return func(user)
+        elif user in known_users:
+            return 'please login'
+        else:
+            return 'please create an account'
+    return wrapper
+
+
+@login_required
+def welcome(user):
+    '''Return a welcome message if logged in'''
+    return f'welcome back {user}'
