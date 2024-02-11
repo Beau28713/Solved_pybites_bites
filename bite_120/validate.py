4247652193d53fe43de36cb3fabe62ad0dff4f7a
Beau Clark <beau28713@gmail.com>
2024-02-11 09:07:59 -0500

    solved bite 120

--- bite_120/validate.py
+++ bite_120/validate.py
@@ -3,4 +3,12 @@ from functools import wraps
 
 def int_args(func):
     @wraps(func)
-    # complete this decorator
+    def wrapper(*args):
+        for arg in args:
+            if not isinstance(arg, int):
+                raise TypeError
+            elif arg < 0:
+                raise ValueError
+        
+        return func(*args)
+    return wrapper
