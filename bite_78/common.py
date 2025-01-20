78c7d504ba3198916beb3da0ad762ae81d9846cb
Beau <beau28713@gmail.com>
2025-01-20 07:39:00 -0500

    implement common_languages function to find common programming languages among programmers

--- bite_78/common.py
+++ bite_78/common.py
@@ -1,4 +1,7 @@
 def common_languages(programmers):
     """Receive a dict of keys -> names and values -> a sequence of
        of programming languages, return the common languages"""
-    pass
+    common_lang = set(programmers[list(programmers.keys())[0]])
+    for programmer in programmers:
+        common_lang = common_lang & set(programmers[programmer])
+    return common_lang
