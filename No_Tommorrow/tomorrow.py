ae8105c9360200141c964ea95a4653c47acc3023
beau28713 <beau28713@gmail.com>
2025-11-18 09:13:01 -0500

    complete tomorrow() function to return the date object for tomorrow

--- No_Tommorrow/tomorrow.py
+++ No_Tommorrow/tomorrow.py
@@ -1,3 +1,8 @@
+import datetime
 def tomorrow(today=None):
+    ###Help me out by completing the tomorrow() function to return a date object with tomorrow's date.###
     # Your code goes here
-    pass
+    if today is None:
+        today = datetime.date(2020, 7, 9)
+    next_day = today + datetime.timedelta(days=1)
+    return next_day
